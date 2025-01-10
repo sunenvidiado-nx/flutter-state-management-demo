b086b9398a457ffdf316104e6eb35bf5a1faa1a4
Sun Adriann Envidiado <sunadriann31@gmail.com>
2025-01-10 16:23:44 +0800

    chore: 💡 Update comment regarding dependency injection

--- apps/weather_mobx_app/lib/src/weather_store.dart
+++ apps/weather_mobx_app/lib/src/weather_store.dart
@@ -6,10 +6,10 @@ part 'weather_store.g.dart';
 class WeatherStore = _WeatherStore with _$WeatherStore;
 
 abstract class _WeatherStore with Store {
-  // In a production app, this would be injected via dependency injection
-  // or a service locator.
-  final _weatherRepository =
-      WeatherRepository(const String.fromEnvironment('WEATHER_API_KEY'));
+  // Typically, you would use a dependency injection framework like GetIt.
+  final _repository = WeatherRepository(
+    const String.fromEnvironment('WEATHER_API_KEY'),
+  );
 
   @observable
   Weather? weather;
@@ -21,7 +21,7 @@ abstract class _WeatherStore with Store {
   Future<void> fetchWeather(String newCity) async {
     isLoading = true;
     try {
-      weather = await _weatherRepository.getWeather(newCity);
+      weather = await _repository.getWeather(newCity);
     } finally {
       isLoading = false;
     }
